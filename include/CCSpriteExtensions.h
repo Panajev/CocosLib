75ee3f58d4e674237520fe025f640013a530f939
Panajev <Panajev@gmail.com>
2011-06-22 00:53:42 +0200

    (+): initial source commit

---
+++ include/CCSpriteExtensions.h
@@ -0,0 +1,13 @@
+//
+//  CCSpriteExtension.h
+//  CocosLib
+//
+//  Created by Goffredo Marocchi on 6/2/11.
+//  Copyright 2011 IGGS. All rights reserved.
+//
+
+#import "cocos2d.h"
+
+@interface CCSprite (Xtensions)
++(id) spriteWithSpriteFrameNameOrFile:(NSString*)nameOrFile;
+@end

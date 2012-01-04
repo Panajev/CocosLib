b3f5ebf477dc0df9eb664c1c8d49a4c3953619df
Panajev <Panajev@gmail.com>
2012-01-05 00:21:55 +0100

    (+): updated to Cocos2D 2.0 Beta 2

--- include/cocos2d/Platforms/iOS/CCDirectorIOS.h
+++ include/cocos2d/Platforms/iOS/CCDirectorIOS.h
@@ -3,17 +3,17 @@
  *
  * Copyright (c) 2008-2010 Ricardo Quesada
  * Copyright (c) 2011 Zynga Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,11 +26,13 @@
 
 // Only compile this code on iOS. These files should NOT be included on your Mac project.
 // But in case they are included, it won't be compiled.
-#import <Availability.h>
-#ifdef __IPHONE_OS_VERSION_MAX_ALLOWED
+#import "../../ccMacros.h"
+#ifdef __CC_PLATFORM_IOS
 
 #import "../../CCDirector.h"
 #import "kazmath/mat4.h"
+
+@class CCTouchDispatcher;
 
 /** CCDirector extensions for iPhone
  */
@@ -49,7 +51,7 @@
 /** Will enable Retina Display on devices that supports it.
  It will enable Retina Display on iPhone4 and iPod Touch 4.
  It will return YES, if it could enabled it, otherwise it will return NO.
- 
+
  This is the recommened way to enable Retina Display.
  @since v0.99.5
  */
@@ -68,8 +70,15 @@
 @interface CCDirectorIOS : CCDirector
 {
 	/* contentScaleFactor could be simulated */
-	BOOL	isContentScaleSupported_;	
+	BOOL	isContentScaleSupported_;
+
+	/* touch dispatcher */
+	CCTouchDispatcher	*touchDispatcher_;
 }
+
+/** Touch Dispatcher */
+@property (nonatomic,readwrite,retain) CCTouchDispatcher * touchDispatcher;
+
 @end
 
 /** DisplayLinkDirector is a Director that synchronizes timers with the refresh rate of the display.
@@ -85,7 +94,8 @@
  */
 @interface CCDirectorDisplayLink : CCDirectorIOS
 {
-	id displayLink;
+	CADisplayLink	*displayLink_;
+	CFTimeInterval	lastDisplayTime_;
 }
 -(void) mainLoop:(id)sender;
 @end
@@ -93,4 +103,4 @@
 // optimization. Should only be used to read it. Never to write it.
 extern CGFloat	__ccContentScaleFactor;
 
-#endif // __IPHONE_OS_VERSION_MAX_ALLOWED
+#endif // __CC_PLATFORM_IOS

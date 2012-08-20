3a86cef58f6d505b5d30d91bfdb55adc6913a1f0
Panajev <Panajev@gmail.com>
2012-08-20 13:33:44 +0200

    (+): implemented performance enhancement.
    https://github.com/cocos2d/cocos2d-iphone/pull/232

--- include/cocos2d/CCActionInterval.h
+++ include/cocos2d/CCActionInterval.h
@@ -396,7 +396,7 @@ typedef struct _ccBezierConfig {
 /** Animates a sprite given the name of an Animation */
 @interface CCAnimate : CCActionInterval <NSCopying>
 {
-	NSMutableArray		*splitTimes_;
+	NSData              *splitTimes_;
 	NSInteger			nextFrame_;
 	CCAnimation			*animation_;
 	id					origFrame_;
